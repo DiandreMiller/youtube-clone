5a0a46c69c2496e2f423bd4e643a401379624819
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-10 07:59:41 -0500

    hamburger menu

--- README.md
+++ README.md
@@ -50,3 +50,7 @@ Skills:
 - Git: A version control system that allows you to track changes to your code and collaborate with others.
 - Local storage: A web technology that allows you to store data on the user's browser so it persists even if the user closes the browser or navigates away from the website.
 - YouTube Data API integration: Incorporating data from remote sources using APIs (Application Programming Interface)
+
+### Resources
+
+Hamburger Menu - [WebDev Basics - React Animated Navbar Burger Menu [2022]]()
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,37 @@
+import React, {useState} from "react";
+import './Header.css'
 
+const Header = () => {
 
-function Header(){
+    // to change burger classes
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+    const [menu_class, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    // toggle burger menu change
+    const updateMenu = () => {
+        if(!isMenuClicked) {
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu visible")
+        }
+        else {
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
 
     return(
-        <div>
-            
+        <div style={{width: '100%', height: '100vh'}}>
+            <nav className="hamburger">
+                <div className="hamburger__menu" onClick={updateMenu}>
+                    <div className={burger_class} ></div>
+                    <div className={burger_class} ></div>
+                    <div className={burger_class} ></div>
+                </div>
+            </nav>
+
+            <div className={menu_class}></div>
         </div>
     )
 }

152fa5913064a38ecc07df685bbf2867876d4a1c
Diandre Miller <millerdiandre@gmail.com>
2023-03-10 13:17:07 -0500

    Fetched API

---
+++ src/API/fetch.jsx
@@ -0,0 +1,21 @@
+const URL = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE}`
+
+
+
+ const displayAllVideos = () => {
+
+    return fetch(`${URL}`)
+        .then(response => response.json())
+    
+
+}
+
+export default displayAllVideos
+
+
+
+
+
+
+
+

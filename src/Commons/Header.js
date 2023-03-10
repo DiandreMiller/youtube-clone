d5ef387ddb031ff22f886f934afcb702dda11ca8
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-10 18:13:03 -0500

    Menu before and after clicking hamburger

--- src/Commons/Header.js
+++ src/Commons/Header.js
@@ -1,19 +1,40 @@
 import React, { useState } from "react";
 import "./Header.css";
 import logo from "../Images/YouTube_Logo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
 
   const updateMenu = () => {
-    setIsMenuClicked(!isMenuClicked);
+    // function to update the menu state
+    setIsMenuClicked(!isMenuClicked); // toggle isMenuClicked to its opposite value
   };
+  // determine the class names for the hamburger menu and the menu based on the isMenuClicked value
+  const burgerClass = isMenuClicked
+    ? "burger-bar clicked"
+    : "burger-bar unclicked";
+  const menuClass = isMenuClicked ? "menu visible" : "hidden";
 
-  const burgerClass = isMenuClicked ? "burger-bar clicked" : "burger-bar unclicked";
-  const menuClass = isMenuClicked ? "menu visible" : "menu hidden";
+  // determine the content for the menu based on the isMenuClicked value
+  const menuContent = isMenuClicked ? (
+    <div className="menu__items">
+      <p>Home</p>
+      <p>Shorts</p>
+      <p>Subscription</p>
+      <p>Library</p>
+    </div>
+  ) : (
+    <div className="menu__items">
+      <p>About</p>
+      <p>Contact</p>
+      <p>Settings</p>
+      <p>Trending</p>
+    </div>
+  );
 
   return (
-    <header style={{ width: "100%", height: "100vh" }}>
+    <header className="hamburger__header">
       <nav className="hamburger">
         <div className="hamburger__navbar">
           <div className="hamburger__menu" onClick={updateMenu}>
@@ -22,23 +43,18 @@ const Header = () => {
             <div className={burgerClass}></div>
           </div>
           <div className="navbar__logo">
-            <img src={logo} alt="YouTube" />
+            <Link to="/Components/Home">
+              <img src={logo} alt="YouTube" />
+            </Link>
+          </div>
+          <div className="navbar__rightIcons">
+            <i className="fas fa-video"></i>
+            <i className="fas fa-bell"></i>
+            <i className="fas fa-user"></i>
           </div>
         </div>
-
-        <div className="navbar__right">
-          <button>
-            <i className="fas fa-video"></i>
-          </button>
-          <button>
-            <i className="fas fa-bell"></i>
-          </button>
-          <button>
-            <i className="fas fa-user"></i>
-          </button>
-        </div>
       </nav>
-      <div className={menuClass}>home</div>
+      <div className={menuClass}> {menuContent} </div>
     </header>
   );
 };

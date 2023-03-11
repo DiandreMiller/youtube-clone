d723255cff3a2acd7a3d4ad932af641b11b95ea1
Diandre Miller <millerdiandre@gmail.com>
2023-03-11 18:17:45 -0500

    loading is semifunctional, got rid of loading file, put the functionality for loading in app.js conditionally

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 import "./App.css";
 import Header from "./Commons/Header";
 import Home from "./Components/Home";
-import Loading from "./Components/Loading";
 import Video from "./Components/Video";
-// import { useEffect, useState } from "react";
-// import displayAllVideos from "./API/fetch";
+import { useEffect, useState } from "react";
+import About from "./Components/About";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClockLoader from "react-spinners/ClockLoader"
 
 function App() {
 
-  // const[videos,setVideos]=useState([])
-  // const[search, setSearch]=useState("")
+  
 
-  // useEffect(() => {
-  //   displayAllVideos()
-  //     .then((result) => {
-  //       setVideos(result)
-  //       console.log(result);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+      setTimeout(() => {
+          setLoading(false)
+          setTimeout(() => {
+              setLoading(false)
+          })
+      }, 3000)
+  }
+      , [])
 
   return (
     <div>
       <BrowserRouter>
-        <Header />
+        
+        
+        {loading ?
+          
+          <div>
+            <h1>Time to Indulge</h1>
+            <ClockLoader
+              loading={loading}
+              color='#d636a9'
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          
+          </div>
+          :
+          <Header />}
         <Video />
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
           {/* <Route path="/" element={<Video />} /> */}
-          {/* <Route path="/" element={<Loading />} /> */}
+          <Route path='/about' element={<About/>} />
+          
         </Routes>
+        
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/About.js
@@ -0,0 +1,5 @@
+const About = () => {
+    return <p>poop</p>
+}
+
+export default About

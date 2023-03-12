fb9f1e87f7298a2c6f68913da42b234e79d303f2
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-12 15:51:50 -0400

    contact form updated

--- src/App.js
+++ src/App.js
@@ -18,76 +18,67 @@ import Contact from "./Components/Contact";
 
 //Spinner
 
-import ClockLoader from "react-spinners/ClockLoader"
+import ClockLoader from "react-spinners/ClockLoader";
 
 //Theme Context
 
 import { createContext } from "react";
-export const ThemeContext = createContext('null');
-
-
+export const ThemeContext = createContext("null");
 
 function App() {
-
-//Loading Spinner
-  const [loading, setLoading] = useState(true)
+  //Loading Spinner
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
       setTimeout(() => {
-          setLoading(false)
-          setTimeout(() => {
-              setLoading(false)
-          })
-      }, 3000)
-  }
-    , [])
-  
+        setLoading(false);
+      });
+    }, 3000);
+  }, []);
+
   //Light/Dark Theme
 
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark')
+    if (theme === "light") {
+      setTheme("dark");
     } else {
-      setTheme('light')
+      setTheme("light");
     }
-  }
+  };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }} >
-    <div className={theme}>
-      <BrowserRouter>
-        
-        
-        {loading ?
-          
-          <div>
-            <h1>Time to Indulge</h1>
-            <ClockLoader
-              loading={loading}
-              color='#d636a9'
-              size={150}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-            />
-          
-          </div>
-            :
-            
-          <Header  />}
-        
-        {loading ? null : <Video />}
-        
-        <Routes>
-          {/* {loading ? null : <Route path='/' element={<Home />} />} */}
-          {loading ? null : <Route path='/about' element={<About />} />}
-          {loading ? null : <Route path='/contact' element={<Contact />} />}
-        </Routes>
-        
-      </BrowserRouter>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={theme}>
+        <BrowserRouter>
+          {loading ? (
+            <div>
+              <h1>Time to Indulge</h1>
+              <ClockLoader
+                loading={loading}
+                color="#d636a9"
+                size={150}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            </div>
+          ) : (
+            <Header />
+          )}
+
+          {loading ? null : <Video />}
+
+          <Routes>
+            {/* {loading ? null : <Route path='/' element={<Home />} />} */}
+            {loading ? null : <Route path="/about" element={<About />} />}
+            {loading ? null : <Route path="/contact" element={<Contact />} />}
+          </Routes>
+        </BrowserRouter>
       </div>
-      </ThemeContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 export default App;
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,73 @@
-const Contact = () => {
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
 
-    return <h1>Contact Us</h1>
-}
+const Result = () => {
+  return (
+    <p>Thank you ❤️ We received your message and will contact you soon.</p>
+  );
+};
+
+const Contact = () => {
+  const [result, setResult] = useState(false);
+  const form = useRef();
+  const sendEmail = (event) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_nj48cb8",
+        "template_vandhana",
+        form.current,
+        "process.env.REACT_APP_EMAIL"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    event.target.reset();
+    setResult(true);
+  };
+
+  // hide result
+  setTimeout(() => {
+    setResult(false);
+  }, 5000);
+
+  return (
+    <div className={result ? "modal-container modal-show" : "modal-container"}>
+      <form className="modal-form" ref={form} onSubmit={sendEmail}>
+        <h2>Say Hello!</h2>
+        <div>
+          <label htmlFor="from_name">Full Name:</label>
+          <br />
+          <input type="text" id="from_name" name="from_name" required />
+        </div>
+        <div>
+          <label htmlFor="from_email">Email:</label>
+          <br />
+          <input type="email" id="from_email" name="from_email" required />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <br />
+          <textarea id="message" name="message" required></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {result && (
+        <div className="modal-overlay" onClick={() => setResult(false)}>
+          <div className="modal-result">
+            <Result />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Contact;

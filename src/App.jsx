9efd13894ae09ed58f9d1cc38ca333e635a2c382
Diandre Miller <millerdiandre@gmail.com>
2023-03-13 20:18:06 -0400

    updated pull request

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+//CSS
+
+import "./App.css";
+import "./DarkMode.css";
+
+//React Hooks
+
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Components
+
+import Header from "./Commons/Header";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import ShowVideo from "./Components/ShowVideo";
+
+//Spinner
+
+import ClockLoader from "react-spinners/ClockLoader";
+
+//Theme Context
+
+import { createContext } from "react";
+export const ThemeContext = createContext("null");
+
+function App() {
+  const [modal, setModal] = useState(false);
+   //Loading Spinner
+   const [loading, setLoading] = useState(true);
+   const [theme, setTheme] = useState("light");
+  //  const [userSearch, setUserSearch] = useState(false);
+
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+ 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      });
+    }, 1000);
+  }, []);
+
+  //Light/Dark Theme
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+     <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={theme}>
+        <BrowserRouter>
+          {loading ? (
+            <div>
+              <h1>Time to Indulge</h1>
+              <ClockLoader
+                loading={loading}
+                color="#d636a9"
+                size={150}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            </div>
+          ) : (
+            <Header modal={modal} toggleModal={toggleModal} />
+          )}
+          <Routes>
+            {loading ? null : (
+              <Route
+                path="/"
+                element={<Home/>}
+              />
+            )}
+            <Route path="/about" element={<About />} />
+            {loading ? null : (
+              <Route path="/contact" element={<Contact modal={modal} toggleModal={toggleModal} />}
+              />
+            )}
+             {loading ? null : (
+            <Route path="/videos/:id" element={<ShowVideo/>}/>
+             )}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+export default App;

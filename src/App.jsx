1f54cbac221c9e3ebabb19dcacdec5a82b3e37b0
Diandre Miller <millerdiandre@gmail.com>
2023-03-15 19:19:39 -0400

    search feature in show video now partially

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,66 @@ function App() {
     }
   };
 
+  //Handle Search
+
+
+  const [isModalMenuOpen, setIsModalMenuOpen] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [search, setSearch] = useState("");
+  const [videos, setVideos] = useState([]);
+  const [videoIds, setVideoIds] = useState([]);
+  
+
+  const URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search}&type=video&key=${process.env.REACT_APP_YOUTUBE}&maxResults=1`;
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    if (!search) {
+      setIsError(true);
+      setErrorMessage("Please enter a search term.");
+      return;
+    }
+    fetch(`${URL}`)
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 403) {
+            setIsError(true);
+            setErrorMessage("Access to YouTube API is forbidden.");
+          } else if (response.status === 400) {
+            setIsError(true);
+            setErrorMessage("Bad request. Please try again later.");
+          } else if (response.status === 404) {
+            setIsError(true);
+            setErrorMessage(
+              "No videos found. Please try a different search query."
+            );
+          } else {
+            setIsError(true);
+            setErrorMessage(
+              "An error occurred while fetching data. Please try again later."
+            );
+          }
+          throw new Error(`HTTP Error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setVideos(data.items.map((item) => item.snippet));
+        setVideoIds(data.items.map((item) => item.id.videoId));
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  //Handle Button Click
+
+  const handleButtonClick = () => {
+    setIsModalMenuOpen(true);
+  };
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className={theme}>
@@ -75,7 +135,23 @@ function App() {
             <Header modal={modal} toggleModal={toggleModal} />
           )}
           <Routes>
-            {loading ? null : <Route path="/" element={<Home />} />}
+            {loading ? null : <Route path="/"
+              element={<Home
+                handleSearch={handleSearch}
+                handleButtonClick={handleButtonClick}
+                URL={URL}
+                isError={isError}
+                setIsError={setIsError}
+                errorMessage={errorMessage}
+                videos={videos}
+                setVideos={setVideos}
+                videoIds={videoIds}
+                setVideoIds={setVideoIds}
+                search={search}
+                setSearch={setSearch}
+                isModalMenuOpen={isModalMenuOpen}
+                setIsModalMenuOpen={setIsModalMenuOpen} />}
+               />}
             {loading ? null : <Route path="/about" element={<About />} />}
             {loading ? null : (
               <Route
@@ -84,7 +160,23 @@ function App() {
               />
             )}
             {loading ? null : (
-              <Route path="/videos/:id" element={<ShowVideo />} />
+              <Route path="/videos/:id"
+                element={<ShowVideo
+                  handleSearch={handleSearch}
+                  handleButtonClick={handleButtonClick}
+                  URL={URL}
+                  isError={isError}
+                  setIsError={setIsError}
+                  errorMessage={errorMessage}
+                  videos={videos}
+                  setVideos={setVideos}
+                  videoIds={videoIds}
+                  setVideoIds={setVideoIds}
+                  search={search}
+                  setSearch={setSearch}
+                  isModalMenuOpen={isModalMenuOpen}
+                  setIsModalMenuOpen={setIsModalMenuOpen}
+                />} />
             )}
           </Routes>
         </BrowserRouter>
--- src/Components/ShowVideo.jsx
+++ src/Components/ShowVideo.jsx
@@ -2,17 +2,81 @@ import YouTube from "react-youtube";
 import "./ShowVideo.css";
 import { useParams} from "react-router-dom";
 import Comments from "./Comments";
+import FutureWorks from "./FutureWorks";
+import Error from "./Error";
 
 
-export default function ShowVideo() {
+const ShowVideo = (handleSearch, errorMessage,
+    setSearch, search,
+    isError, setIsError, isModalMenuOpen, setIsModalMenuOpen, handleButtonClick ) => {
     const { id } = useParams();
     return (
         <div className="showVideo">
-        <div className="video">
+            <div className="video">
+            <div className="video-container">
+      {isError && <Error setIsError={setIsError} errorMessage={errorMessage} />}
+      <div className="video-wrapper">
+        <form onSubmit={handleSearch} className="search__Form">
+          <div className="search-container">
+            <input
+              type="text"
+              value={search}
+              onChange={(event) => setSearch(event.target.value)}
+              autoFocus
+              placeholder="Search"
+              className="search-input"
+              title="Enter a search value"
+            />
+            <button type="submit" className="search-button" title="Search">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+        </form>
+        <div className="buttons-container">
+          <button className="search-option-button" onClick={handleButtonClick}>
+            All
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Music
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Cooking
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Programming
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Gaming
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Awards
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Auditions
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Comedy
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Baseball
+          </button>
+          <button className="search-option-button" onClick={handleButtonClick}>
+            Drawing
+          </button>
+          {isModalMenuOpen && (
+            <FutureWorks setIsModalMenuOpen={setIsModalMenuOpen} />
+          )}
+        </div>
+
+      </div>
+    </div>
+
             <YouTube videoId={id} />
         </div>
         <Comments/>
         </div>
     )
 }
+
+export default ShowVideo
 
